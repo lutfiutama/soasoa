62d862dba2c11e88e3d0ee63313f2203b8b11719
lutfiutama <38356749+lutfiutama@users.noreply.github.com>
2018-04-13 23:29:23 +0700

    Create delete-testing.php

---
+++ delete-testing.php
@@ -0,0 +1,17 @@
+<?php
+include 'conn.php';
+//Delete record from database
+
+$query = "DELETE FROM `user` WHERE   `id`='1'";
+if ($connection->query($query)) {
+    $msg = array("status" =>1 , "msg" => "Record Deleted successfully");
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($connection);
+} 
+
+$json = $msg;
+
+header('content-type: application/json');
+echo json_encode($json);
+
+@mysqli_close($conn);
